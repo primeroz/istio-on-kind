25f69f298d67c95bdea0fd11a4605efa247a6d90
Francesco Ciocchetti <primeroznl@gmail.com>
2021-01-28 12:42:38 +0100

    Reorg and fix operator yamls

--- setup-simple15.sh
+++ setup-simple15.sh
@@ -1,36 +1,15 @@
 #!/bin/bash
 
-git submodule update --init
-
-istioctl15 operator init
+kubectl create ns istio-operator
+kubectl apply -f operator/1.5.10/operator.yaml
+sleep 10
+kubectl wait -n istio-operator deployment --all --for=condition=available --timeout=180s
 
 # Create control plane 
 kubectl create ns istio-system
 kubectl ns istio-system
-kubectl apply -f - <<EOF
-apiVersion: install.istio.io/v1alpha1
-kind: IstioOperator
-metadata:
-  namespace: istio-system
-  name: example-istiocontrolplane
-spec:
-  profile: default
-  addonComponents:
-    grafana:
-      enabled: false
-    kiali:
-      enabled: false
-    prometheus:
-      enabled: false
-  values:
-    global:
-      tracer:
-        zipkin:
-          address: zipkin.istio-system:9411
-EOF
+kubectl apply -f operator/1.5.10/crd.yaml
 
-sleep 5
-kubectl wait -n istio-operator deployment --all --for=condition=available --timeout=180s
 sleep 30
 kubectl wait -n istio-system deployment --all --for=condition=available --timeout=180s
 sleep 5
@@ -41,21 +20,21 @@ kubectl wait -n istio-system deployment --all --for=condition=available --timeou
 kubectl create ns istio-demo
 kubectl label namespace istio-demo istio-injection=enabled
 kubectl ns istio-demo
-kubectl apply -f istio/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f bookinfo/bookinfo-kube/bookinfo.yaml
 kubectl wait -n istio-demo deployment --all --for=condition=available --timeout=180s
 
-kubectl apply -f istio/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f bookinfo/bookinfo-networking/bookinfo-gateway.yaml
 
-kubectl apply -f istio/samples/bookinfo/networking/destination-rule-all-mtls.yaml 
+kubectl apply -f bookinfo/bookinfo-networking/destination-rule-all-mtls.yaml 
 
 
 
 # Observability Stack
 
-kubectl apply -f istio/samples/addons 
-kubectl apply -f prometheus-configmap.yml
+kubectl apply -f istio-addons 
+kubectl apply -f istio-addons/extras/prometheus-configmap.yml
 sleep 2
-kubectl apply -f istio/samples/addons 
+kubectl apply -f istio-addons 
 kubectl rollout status deployment/kiali -n istio-system
 
 #kubectl apply -f istio/samples/addons/extras/zipkin.yaml
