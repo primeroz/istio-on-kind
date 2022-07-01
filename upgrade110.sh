b001ffeed394b99d7e7e17e4525b4e98016b6e30
Francesco Ciocchetti <primeroznl@gmail.com>
2022-07-01 11:24:36 +0200

    chore: add new versions for istio

---
+++ upgrade110.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ "x${ENV}" != "x" ]]; then
+source ./env-${ENV}.sh
+else
+source ./env-default.sh
+fi
+
+kubectl apply -f operator/1.10.6/operator.yaml
+CLUSTER_DNS_DOMAIN=${CLUSTER_DNS_DOMAIN} envsubst < operator/1.10.6/crd.yaml | kubectl apply -f -
+kubectl wait -n istio-operator deployment --all --for=condition=available --timeout=180s
+sleep 60
+kubectl wait -n istio-system deployment --all --for=condition=available --timeout=180s
+# wait
+sleep 30
+kubectl wait -n istio-system deployment --all --for=condition=available --timeout=180s
+kubectl rollout restart -n istio-demo deployment
+kubectl wait -n istio-demo deployment --all --for=condition=available --timeout=180s
+# wait
+istioctl proxy-status
